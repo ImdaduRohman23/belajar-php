08f315c58dd3b7f5067cc23d6a9f0bbbed9d27d8
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-11-14 13:52:52 +0700

    pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,74 @@
+<?php
+use function CommonMark\Render\HTML;
+
+// ========== Pertemuan 2-PHP ==========
+// #Sintaks PHP
+
+
+// #Standar Output
+// //1. echo
+// echo "Imdadu Rohman with echo";
+// //2. print
+// print "Imdadu Rohman with print";
+// //3. print_r
+// print_r("Imdadu Rohman with print_r");
+// //4. var_dump
+// var_dump("Imdadu Rohman with var_dump");
+
+
+// #Penulisan Sintaks
+// 1. PHP di dalam HTML => paling sering dipake
+// 2. HTML di dalam PHP => tidak disarankan
+
+
+// #Variabel dan Tipe Data
+// $nama = "imdadu";
+// echo "<h1> halo, $nama </h1>";
+// echo 'halo, $nama';
+
+
+// #Operator
+// + - / %
+// echo 1 + 2;
+// $angka1 = 1;
+// $angka2 = 2;
+// echo $angka1 + $angka2;
+
+
+// #Penggabung String / Concatenitation / Concat
+// $nama_depan = "Imdadu";
+// $nama_belakang = "Rohman";
+// echo $nama_depan . " " . $nama_belakang;
+
+
+// #Asignment
+// = += -= *= /= %= .=
+// $a = 1;
+// $a += 5;
+// echo $a;
+
+// $nama_depan = "Imdadu";
+// $nama_depan .= " ";
+// $nama_depan .= "Rohman";
+// echo $nama_depan;
+
+
+// #Perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 > 8);
+// var_dump(1 == '1');
+
+
+// #Identitas
+// === !==
+// var_dump(1 === '1');
+
+
+// #Logika
+// && || !
+// $a = 10;
+// var_dump($a < 5 && $a % 2 == 0);
+// var_dump($a < 5 || $a % 2 == 0);
+
+
+?>
