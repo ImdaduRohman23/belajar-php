35fc627bb0f8f328ceac20638f6d206b84457080
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-11-15 13:45:16 +0700

    pertemuan 3

---
+++ pertemuan3/empl1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Emplemetasi 1</title>
+</head>
+
+<body>
+    <table border="1" cellpadding='10' cellspacing='0'>
+        <tr>
+            <td>1</td>
+            <td>2</td>
+        </tr>
+    </table>
+</body>
+
+</html>
---
+++ pertemuan3/latihan1.php
@@ -0,0 +1,35 @@
+<?php
+// #PENGULANGAN
+
+// 1. for
+// for ($i = 0; $i < 5; $i++) {
+//     echo 'oii <br>';
+// }
+
+// 2. while
+// $i = 0;
+// while ($i < 10) {
+//     echo 'oii <br>';
+//     $i++;
+// }
+
+// 3.do while => bedanya sama while, minimal satu kali dijalankan donya.
+// $i = 20;
+// do {
+//     echo 'oii <br>';
+//     $i++;
+// } while ($i < 5);
+
+// #PENGONDISIAN
+// if else if else
+
+$x = 21;
+if ($x < 10) {
+    echo 'salah';
+} else if ($x == 20) {
+    echo 'tepat';
+} else {
+    echo 'benar';
+}
+
+?>
