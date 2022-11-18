3825f4af1291aa63a0edb72ccd0dfcc1f4b0ee61
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-11-18 14:34:52 +0700

    pertemuan 3 => implementasi

--- pertemuan3/empl1.php
+++ pertemuan3/empl1.php
@@ -9,11 +9,53 @@
 </head>
 
 <body>
+    <h3>Cara Manual</h3>
     <table border="1" cellpadding='10' cellspacing='0'>
         <tr>
-            <td>1</td>
-            <td>2</td>
+            <td>1,1</td>
+            <td>1,2</td>
+            <td>1,3</td>
         </tr>
+        <tr>
+            <td>2,1</td>
+            <td>2,2</td>
+            <td>2,3</td>
+        </tr>
+        <tr>
+            <td>3,1</td>
+            <td>3,2</td>
+            <td>3,3</td>
+        </tr>
+    </table>
+    <hr>
+    <hr>
+
+    <h3>Dengan Looping Biasa</h3>
+    <table border="1" cellpadding='10' cellspacing='0'>
+        <?php
+        for ($i = 1; $i <= 3; $i++) {
+            echo '<tr>';
+            for ($j = 1; $j <= 3; $j++) {
+                echo "<td>$i, $j </td>";
+            }
+            echo '</tr>';
+        }
+        ?>
+    </table>
+    <hr>
+    <hr>
+
+    <h3>Dengan Looping Templating</h3>
+    <table border="1" cellpadding='10' cellspacing='0'>
+        <?php for ($i = 1; $i <= 3; $i++) { ?>
+        <tr>
+            <?php for ($j = 1; $j <= 3; $j++) { ?>
+            <td>
+                <?php echo "$i , $j" ?>
+            </td>
+            <?php } ?>
+        </tr>
+        <?php } ?>
     </table>
 </body>
 
